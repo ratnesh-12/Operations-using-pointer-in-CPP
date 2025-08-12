a808e562d957f36cdaa3be151e66c51f1eba5ee6
Ratnesh Kumar <ratneshkr129@gmail.com>
2025-08-12 15:01:05 +0530

    Create Call by value.cpp

---
+++ Call by value.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int temp;
+void swap(int *x, int *y){
+    temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+int main() {
+    int a = 12, b = 6;
+    cout << "Before swapping: a = " << a << ", b = " << b << endl;
+    swap(a,b);
+    cout << "After swapping: a = " << a << ", b = " << b << endl;
+    return 0;
+}
+
+OUTPUT: 
+Before swapping: a = 12, b = 6
+After swapping: a = 6, b = 12
+
+=== Code Execution Successful ===
